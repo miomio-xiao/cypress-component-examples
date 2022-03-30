1ea78dd3b0f52f337c38f880507038fbe204b487
miomio-xiao <miomio.xiao@gmail.com>
2022-03-30 23:27:37 +0800

    cm2

--- vite-vue/src/components/rate/index.tsx
+++ vite-vue/src/components/rate/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref, Ref } from 'vue';
+import { defineComponent, ref, Ref, watch } from 'vue';
 import { renderIcon } from './icon';
 import './index.css';
 
@@ -7,25 +7,52 @@ const getNumberList = (num: number) => {
 };
 function useRateClasses({
   currentValue,
+  currentOverValue,
   index,
 }: {
   currentValue: Ref<number>;
+  currentOverValue: Ref<number>;
   index: number;
 }) {
-  return ['mio-rate-item', currentValue.value >= index ? 'is-active' : ''];
+  return [
+    'mio-rate-item',
+    (currentOverValue.value || currentValue.value) >= index ? 'is-active' : '',
+  ];
 }
 
 export default defineComponent({
   name: 'Rate',
+  props: {
+    modelValue: {
+      type: Number,
+      default: 0,
+    },
+  },
 
-  setup() {
-    const currentValue = ref(0);
+  setup(props, { emit }) {
+    const currentValue = ref(props.modelValue);
+
+    watch(currentValue, (value) => {
+      emit('update:modelValue', value);
+    });
+
+    watch(
+      () => props.modelValue,
+      (value) => {
+        currentValue.value = value;
+      },
+    );
+
+    const currentOverValue = ref(0);
+
     const onClickItem = (index: number) => (currentValue.value = index);
 
     const renderRateItem = (index: number) => (
       <div
-        class={useRateClasses({ currentValue, index })}
+        class={useRateClasses({ currentValue, currentOverValue, index })}
         onClick={() => onClickItem(index)}
+        onMouseenter={() => (currentOverValue.value = index)}
+        onMouseleave={() => (currentOverValue.value = 0)}
       >
         {renderIcon()}
       </div>
--- vite-vue/src/components/rate/rate.spec.ts
+++ vite-vue/src/components/rate/rate.spec.ts
@@ -1,4 +1,4 @@
-import { mount } from '@cypress/vue';
+import { mount, mountCallback } from '@cypress/vue';
 import Rate from './index';
 
 describe('rate component', () => {
@@ -16,5 +16,49 @@ describe('rate component', () => {
     cy.get('.mio-rate-item:nth-of-type(3)').click();
 
     cy.get('.mio-rate-item.is-active').should('have.length', 3);
+  });
+
+  describe('v-model value', () => {
+    beforeEach(
+      mountCallback(Rate, {
+        propsData: {
+          modelValue: 3,
+        },
+      }),
+    );
+
+    it('should work when set props value', () => {
+      cy.get('.mio-rate-item.is-active')
+        .should('have.length', 3)
+        .then(() => {
+          Cypress.vueWrapper.setProps({
+            modelValue: 4,
+          });
+          cy.get('.mio-rate-item.is-active').should('have.length', 4);
+        });
+    });
+
+    it('should be emit input when the element is clicked', () => {
+      cy.get('.mio-rate-item.is-active').should('have.length', 3);
+
+      cy.get('.mio-rate-item:nth-of-type(2)')
+        .click()
+        .then(() => {
+          expect(Cypress.vueWrapper.emitted()['update:modelValue'].length).to.eq(1);
+          expect(Cypress.vueWrapper.emitted()['update:modelValue'][0][0]).to.eq(2);
+        });
+    });
+  });
+
+  it('should be highlighted when hover', () => {
+    mount(Rate);
+
+    cy.get('.mio-rate-item:nth-of-type(3)').trigger('mouseenter');
+
+    cy.get('.mio-rate-item.is-active').should('have.length', 3);
+
+    cy.get('.mio-rate-item:nth-of-type(3)').trigger('mouseleave');
+
+    cy.get('.mio-rate-item.is-active').should('have.length', 0);
   });
 });
